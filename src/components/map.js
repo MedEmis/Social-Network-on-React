307159fbc4de8925a4c7f68743a49f542dc4267d
MedEmis <draganovmaksim9@gmail.com>
2020-11-20 20:50:19 +0200

    initial comment for react-practice

---
+++ src/components/map.js
@@ -0,0 +1,29 @@
+class map extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			
+		);
+	}
+}
+
+
+class map extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {  };
+	}
+	render() {
+		return (
+			
+		);
+	}
+}
+
+export default map;
+
+
+export default map;
